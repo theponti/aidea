dc24db3c768fcd8eaa14a97ba4a1b31a141fc71d
Charles Ponti <hackefeller@icloud.com>
2020-07-05 05:56:52 -0400

    config: Update eslint & jest settings
    * Add ESLint using Standard
    * Add Jest settings, including those for coverage
    * Add @reach/router

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    "react-app",
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    camelcase: 0
+  }
+}
