2b1184403ca8cda9a650c6ff8c5a4283b64d6fc0
charlesponti <cj@ponti.io>
2022-07-06 12:21:22 -0700

    fix(components/Header): Fix header tests

--- src/containers/App/components/Header/Header.test.jsx
+++ src/containers/App/components/Header/Header.test.jsx
@@ -1,13 +1,34 @@
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import Header from "./Header";
 
+let isAuthenticated;
+
+vi.mock("@auth0/auth0-react", () => ({
+  useAuth0: vi.fn(() => ({ isAuthenticated })),
+}));
+
 describe("<Header/>", () => {
-  it("should render component", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should render unauthenticated header", () => {
+    isAuthenticated = false;
     const { container } = render(
       <BrowserRouter>
-        <Header login={vi.fn()} />
+        <Header />
+      </BrowserRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render authenticated header", () => {
+    isAuthenticated = true;
+    const { container } = render(
+      <BrowserRouter>
+        <Header />
       </BrowserRouter>
     );
     expect(container).toMatchSnapshot();
