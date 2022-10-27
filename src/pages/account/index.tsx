7b0909adccd6736a8fad7fde621ba6dd2800f384
charlesponti <cj@ponti.io>
2022-10-26 19:21:37 -0700

    feat(components/AlertError): create and use AlertError component

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -3,6 +3,7 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/future/image";
 import { useCallback } from "react";
 
+import AlertError from "src/components/AlertError";
 import { getProtectedServerSideProps } from "src/utils";
 import { trpc } from "src/utils/trpc";
 
@@ -44,28 +45,7 @@ const Account: NextPage = () => {
 
         <div className="mt-8 divider" />
 
-        {deleteUser.error && (
-          <div className="alert alert-error shadow-lg">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current flex-shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>
-                <p>Something went wrong! {deleteUser.error.message}</p>
-              </span>
-            </div>
-          </div>
-        )}
+        {deleteUser.error && <AlertError error={deleteUser.error.message} />}
 
         <button className="btn btn-ghost text-error" onClick={onDelectAccount}>
           Delete account
