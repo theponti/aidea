3f5be0285dbf11a7854c4cc6e65eeffe8afeb77a
charlesponti <cj@ponti.io>
2022-10-26 23:59:31 -0700

    style(account): fix layout of account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -22,33 +22,35 @@ const Account: NextPage = () => {
 
   return (
     <PageWrap>
-      <h1>Account</h1>
+      <div className="col-span-12">
+        <h1>Account</h1>
 
-      <div className="card bg-neutral-focus shadow-xl md:max-w-sm">
-        <div className="card-body place-items-center">
-          <Image
-            alt="profile picture"
-            src={pictureUrl.replace("_normal", "")}
-            width={100}
-            height={100}
-            className="avatar rounded-full border-4 border-base-content border-solid w-28 h-28"
-          />
-          <p className="text-lg">{session?.user?.name}</p>
+        <div className="card bg-neutral-focus shadow-xl md:max-w-sm">
+          <div className="card-body place-items-center">
+            <Image
+              alt="profile picture"
+              src={pictureUrl.replace("_normal", "")}
+              width={100}
+              height={100}
+              className="avatar rounded-full border-4 border-base-content border-solid w-28 h-28"
+            />
+            <p className="text-lg">{session?.user?.name}</p>
+          </div>
         </div>
+        <div className="flex flex-col mb-12"></div>
+
+        <button className="btn btn-ghost" onClick={() => signOut()}>
+          Log out
+        </button>
+
+        <div className="mt-8 divider" />
+
+        {deleteUser.error && <AlertError error={deleteUser.error.message} />}
+
+        <button className="btn btn-ghost text-error" onClick={onDelectAccount}>
+          Delete account
+        </button>
       </div>
-      <div className="flex flex-col mb-12"></div>
-
-      <button className="btn btn-ghost" onClick={() => signOut()}>
-        Log out
-      </button>
-
-      <div className="mt-8 divider" />
-
-      {deleteUser.error && <AlertError error={deleteUser.error.message} />}
-
-      <button className="btn btn-ghost text-error" onClick={onDelectAccount}>
-        Delete account
-      </button>
     </PageWrap>
   );
 };
