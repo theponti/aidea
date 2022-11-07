c7d49f3c79d8ac9d8f535be572da662e5ab5c0cf
charlesponti <cj@ponti.io>
2022-11-06 18:24:14 -0800

    fix(DashboardNav): use black for decoration of active links

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -1,21 +1,23 @@
 import classNames from "classnames";
 import Link from "next/link";
 import { NextRouter } from "next/router";
+import React from "react";
 
-export default function DashboardNav({ router }: { router: NextRouter }) {
-  const links = [
-    { path: "/dashboard", text: "Ideas" },
-    { path: "/dashboard/recommendations", text: "Recommendations" },
-  ];
+const LINKS = [
+  { path: "/dashboard", text: "Ideas" },
+  { path: "/dashboard/recommendations", text: "Recommendations" },
+];
+
+function DashboardNav({ router }: { router: NextRouter }) {
   return (
     <div className="col-start-1 col-span-12 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8 flex gap-4 py-4">
-      {links.map((link) => (
+      {LINKS.map((link) => (
         <Link key={link.path} href={link.path}>
           <a
             href={link.path}
             className={classNames(
               router.pathname === link.path &&
-                "underline underline-offset-4 text-primary decoration-neutral-content"
+                "underline underline-offset-4 decoration-black text-primary"
             )}
           >
             {link.text}
@@ -25,3 +27,5 @@ export default function DashboardNav({ router }: { router: NextRouter }) {
     </div>
   );
 }
+
+export default React.memo(DashboardNav);
