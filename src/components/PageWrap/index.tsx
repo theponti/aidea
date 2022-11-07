d9ad5d715932a3a1b33b64d246441a631bf58296
charlesponti <cj@ponti.io>
2022-11-06 18:21:05 -0800

    fix(PageWrap): add margin top and bottom

--- src/components/PageWrap/index.tsx
+++ src/components/PageWrap/index.tsx
@@ -9,7 +9,7 @@ function PageWrap({ className, children }: PageWrapProps) {
   return (
     <div
       className={classNames(
-        "pt-20 col-start-2 col-span-10 grid grid-cols-12 grid-rows-[min-content]",
+        "col-start-2 col-span-10 grid grid-cols-12 grid-rows-[min-content] my-20",
         className && {
           [className]: className,
         }
