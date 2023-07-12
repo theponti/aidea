efaab8af1bd234087ff312466c6c479d10c99de5
charlesponti <cj@ponti.io>
2023-07-12 10:38:50 -0700

    fix(components/DashboardNav): remove anchor tag

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -5,9 +5,9 @@ import React from "react";
 
 const LINKS = [
   { path: "/ideas", text: "Ideas" },
+  { path: "/bookmarks", text: "Bookmarks" },
   { path: "/lists", text: "Lists" },
   { path: "/lists/invites", text: "Invites" },
-  { path: "/bookmarks", text: "Bookmarks" },
 ];
 
 function DashboardNav() {
@@ -15,16 +15,15 @@ function DashboardNav() {
   return (
     <div className="flex gap-4 py-4 mt-2">
       {LINKS.map((link) => (
-        <Link key={link.path} href={link.path}>
-          <a
-            href={link.path}
-            className={classNames(
-              router.pathname === link.path &&
-                "underline underline-offset-4 decoration-black text-primary font-bold decoration-2"
-            )}
-          >
-            {link.text}
-          </a>
+        <Link
+          key={link.path}
+          href={link.path}
+          className={classNames(
+            router.pathname === link.path &&
+              "underline underline-offset-4 decoration-black text-primary font-bold decoration-2",
+          )}
+        >
+          {link.text}
         </Link>
       ))}
     </div>
