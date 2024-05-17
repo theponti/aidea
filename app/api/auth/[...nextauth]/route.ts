b9c711e5eb7aa824b5b13bebf0995d4f53241253
Charles Ponti <cj@ponti.io>
2024-05-17 13:43:36 -0700

    fix(apps): remove edge from next-auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import { authOptions } from "@/server/auth";
 import NextAuth from "next-auth";
 
-export const runtime = "edge";
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
